fbd598a2e66183e0eba2498bdb87955b3adf17f7
rajgokul-it21 <rajgokul.it21@gmail.com>
2021-12-20 04:33:27 +0000

    commit-msg

---
+++ cal.py
@@ -0,0 +1,16 @@
+a=int(input("NUMBER1:"))
+b=int(input("NUMBER2:"))
+def add():
+ print(a+b)
+def sub():
+ print(a-b)
+def mul():
+ print(a*b)
+def div():
+ print(a/b)
+
+add()
+sub()
+mul()
+div()
+
